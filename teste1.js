99f4ec7e2b7d2dce5b3a5b899e489e91082fefe0
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-06-11 12:10:42 -0300

    Fix: alteração em sua função principal

--- teste1.js
+++ teste1.js
@@ -1,21 +1,21 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res, next) => {
+    const name = req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
+    for (let i = 0; i < data.length; i += 1) {
+        if (data[i].name === name) {
             res.send(data[i]);
+            return; // Terminar a execução do loop após encontrar o usuário
         }
     }
 
+    // Se nenhum usuário for encontrado, envia uma resposta de usuário não encontrado
+    res.status(404).send("Usuário não encontrado");
 };
 
-const getUsers = ( req, res, next ) => {
-    
+const getUsers = (req, res, next) => {
     res.send(data);
-    
 };
 
 module.exports = {
