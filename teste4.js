aa03cc7224499546d53bf085375415401d3fb3e9
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-06-11 13:58:32 -0300

    Fix: Correção da logica de função e teste

--- teste4.js
+++ teste4.js
@@ -1,13 +1,16 @@
-var data =  require("./fakeData");
+var data = require("./fakeData");
 
-module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+module.exports = function (req, res) {
+    var id = req.query.id;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+    const user = data.find((d) => d.id === parseInt(id));
 
-    res.send(reg);
+    if (user) {
+        user.name = req.body.name;
+        user.job = req.body.job;
 
+        res.send(user);
+    } else {
+        res.status(404).send("Usuário não encontrado");
+    }
 };
---
+++ teste4.test.js
@@ -0,0 +1,36 @@
+const updateUser = require('./teste4'); 
+test('Atualiza os dados de um usuário específico', () => {
+    const req = {
+        query: { id: '1' },
+        body: { name: 'Novo Nome', job: 'Nova Profissão' }
+    };
+
+    const res = {
+        send: jest.fn()
+    };
+
+    updateUser(req, res);
+
+    expect(res.send).toHaveBeenCalledWith({
+        id: 1,
+        name: 'Novo Nome',
+        job: 'Nova Profissão'
+    });
+});
+
+test('Retorna uma resposta de erro quando o usuário não é encontrado', () => {
+    const req = {
+        query: { id: '99' },
+        body: { name: 'Novo Nome', job: 'Nova Profissão' }
+    };
+
+    const res = {
+        status: jest.fn(() => res),
+        send: jest.fn()
+    };
+
+    updateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledWith('Usuário não encontrado');
+});
