7ab1fb77e7baa86b381c9db252d5fe77c79af589
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-06-11 14:09:46 -0300

    Fix: Melhoria e alteração no código + teste

--- teste5.js
+++ teste5.js
@@ -1,9 +1,14 @@
+const data = require("./fakeData");
 
+module.exports = function (req, res) {
+    const name = req.query.name;
+    let count = 0;
 
-module.exports = function(req, res){
-    
-    var name =  req.query.name;
+    for (let i = 0; i < data.length; i += 1) {
+        if (data[i].name === name) {
+            count += 1;
+        }
+    }
 
-    res.send("Usuário " +  name  + "  foi lido 0 vezes.");
-
+    res.send("Usuário " + name + " foi lido " + count + " vezes no teste1.");
 };
---
+++ teste5.test.js
@@ -0,0 +1,25 @@
+const getUserCount = require("./teste5");
+
+// Mock para fakeData
+jest.mock("./fakeData", () => [
+    { name: "John" },
+    { name: "John" },
+    { name: "Bob" },
+]);
+
+// Mock para req e res
+const req = { query: { name: "John" } };
+const res = { send: jest.fn() };
+
+describe("Teste do arquivo teste5", () => {
+    test("Deve retornar o número correto de vezes que o usuário foi lido no teste1", () => {
+        getUserCount(req, res);
+        expect(res.send).toHaveBeenCalledWith("Usuário John foi lido 2 vezes no teste1.");
+    });
+
+    test("Deve retornar 0 quando o usuário não for encontrado no teste1", () => {
+        req.query.name = "Alice";
+        getUserCount(req, res);
+        expect(res.send).toHaveBeenCalledWith("Usuário Alice foi lido 0 vezes no teste1.");
+    });
+});
